4f02a10cbe7955f281396a2812e9da606faeb2a7
MrElmurodjon <mrelmurodjon@github.com>
2026-08-25 15:32:06 +0500

    Fix encoding in sold_products.html

---
+++ deploy_fix.py
@@ -0,0 +1,23 @@
+import paramiko
+
+host = "83.69.135.18"
+port = 22
+username = "user"
+password = "A6ciWF8m9oR7wcG8WaQ6"
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, port, username, password)
+
+commands = [
+    "cd /var/www/savdo247 && sudo git fetch origin && sudo git reset --hard origin/main",
+    "sudo systemctl restart savdo247"
+]
+
+for cmd in commands:
+    stdin, stdout, stderr = client.exec_command(cmd, get_pty=True)
+    stdin.write(password + "\n")
+    stdin.flush()
+    print("OUT:", stdout.read().decode())
+
+client.close()
